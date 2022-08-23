8640c5baea05eb8b3a51c75338289f25515f1667
Fabio Zanichelli <67701902+Zanicus2000@users.noreply.github.com>
2022-08-24 01:06:13 +0200

    Messa a punto db e schermata admin

--- esame/admin.py
+++ esame/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from esame.models import Battute
+from esame.models import Battute, Recensioni, ProfiloDettagliato, Followers
 
 admin.site.register(Battute)
+admin.site.register(ProfiloDettagliato)
+admin.site.register(Recensioni)
+admin.site.register(Followers)
