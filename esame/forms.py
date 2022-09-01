018ab87616b042dfb13a1dfbb1faabad157fa04a
Fabio Zanichelli <67701902+Zanicus2000@users.noreply.github.com>
2022-09-01 18:10:51 +0200

    Miglioria grafica e miglioramento filtro con tipi mancanti

--- esame/forms.py
+++ esame/forms.py
@@ -5,7 +5,9 @@ class FormFiltro(forms.Form):
     SCELTE = (('Barzelletta', 'Barzellette'),
               ('Battuta squallida', 'Battute squallide'),
               ('Satira', 'Satira'),
-              ('Black humor', 'Black humor'))
+              ('Black humor', 'Black humor'),
+              ('Notizia divertente', 'Notizie divertenti'),
+              ('Gioco di parole', 'Giochi di parole'))
 
     tipo = forms.ChoiceField(required=True, choices=SCELTE, label='Tipo')
 
